961322ac87641366f4d8cb81c5752fd3d4199790
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-24 10:23:09 -0700

    add display error message when response fails

--- src/main.js
+++ src/main.js
@@ -7,10 +7,14 @@ import {apiCall} from "./apiCall.js";
 $(document).ready(function(){
   $("#convert").submit(function(event){
     event.preventDefault();
-    let usDollars = parseInt($("#us-dollars").val());
+    let usDollars = parseInt($("#us-dollars").val()).toFixed(2);
     apiCall().then(function(response){
-      let covertedCurrency = response.conversion_rates.JPY * usDollars;
-      $(".result").html(covertedCurrency);
+      if(response){
+        let covertedCurrency = response.conversion_rates.JPY * usDollars;
+        $(".result").html(covertedCurrency);
+      } else {
+        $(".result").text("There was an error handling your request.");
+      }
     });
   });
 });
