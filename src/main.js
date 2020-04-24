9a475743edd38beb2d11806228aa0f0288bf3326
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-24 14:24:43 -0700

    add currency code next to converted currency and show results when convert button is clicked

--- src/main.js
+++ src/main.js
@@ -7,12 +7,15 @@ import { apiCall } from "./apiCall.js";
 $(document).ready(function () {
   $("#convert").submit(function (event) {
     event.preventDefault();
+    $(".result").show();
     let usDollars = parseInt($("#us-dollars").val()).toFixed(2);
     let currencyType = $("#currency-type").val();
     apiCall().then(function (response) {
       if (response) {
         let covertedCurrency = response.conversion_rates[currencyType] * usDollars;
-        $(".result").html(covertedCurrency);
+        $("#currency-code").html(currencyType);
+        console.log(currencyType);
+        $("#converted-currency").html(covertedCurrency);
         if (!response.conversion_rates[currencyType]) {
           $(".result").text("Does not exist.");
         } 
