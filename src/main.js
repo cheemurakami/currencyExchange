3bd88a5435b0caca77abeba15c96ae1600c20c5c
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-24 15:12:36 -0700

    add display last update time and next update time

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import $ from "jquery";
 import { apiCall } from "./apiCall.js";
 
 $(document).ready(function () {
+  apiCall().then(function(response){
+    if(response){
+      
+      let lastUpdate = new Date(response.time_last_update * 1000).toString();
+      let nextUpdate = new Date(response.time_next_update * 1000).toString();
+      console.log(lastUpdate)
+      $("#last-update").html(`Last update: ${lastUpdate}`);
+      $("#next-update").html(`Next update: ${nextUpdate}`);
+    }
+  });
   $("#convert").submit(function (event) {
     event.preventDefault();
     $(".result").show();
